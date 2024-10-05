2b4f7599a0e76e800e75bd658666a030dd976db5
Vitaliy Posvistak <85239660+vitaliy65@users.noreply.github.com>
2024-10-05 18:55:52 +0300

    created
    created task#3

---
+++ Task#03/Task#03/RenderControl/RenderControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+
+namespace Task_03
+{
+    public partial class RenderControl : OpenGL
+    {
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
